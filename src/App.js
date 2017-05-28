f4b217003f20696fa78457f3627321cb6bde1398
John Crisostomo <johncrisostomo@protonmail.ch>
2017-05-28 10:00:23 +0800

    imports reusable components and rebuilds the LoginForm

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import { createStore } from 'redux';
 import firebase from 'firebase';
 import reducers from './reducers';
 import settings from '../firebaseSettings';
+import LoginForm from './components/LoginForm';
 
 class App extends Component {
   componentWillMount() {
@@ -15,7 +16,7 @@ class App extends Component {
     return (
       <Provider store={createStore(reducers)}>
         <View>
-          <Text>Hello!</Text>
+          <LoginForm />
         </View>
       </Provider>
     )
