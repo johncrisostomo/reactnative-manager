765cb76b5ad5b4c481bee5cd393a73119ea12224
John Crisostomo <johncrisostomo@protonmail.ch>
2017-05-28 14:13:58 +0800

    handles LOGIN_USER_SUCCESS properly

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,5 +1,9 @@
 import firebase from 'firebase';
-import { EMAIL_CHANGED, PASSWORD_CHANGED } from './types';
+import {
+  EMAIL_CHANGED,
+  PASSWORD_CHANGED,
+  LOGIN_USER_SUCCESS,
+ } from './types';
 
 export const emailChanged = (text) => {
   return {
@@ -18,6 +22,8 @@ export const passwordChanged = (text) => {
 export const loginUser = ({ email, password }) => {
   return (dispatch) => {
     firebase.auth().signInWithEmailAndPassword(email, password)
-      .then(user => dispatch({ type: 'LOGIN_USER_SUCCESS', payload: user }));
+      .then(user => {
+        dispatch({ type: LOGIN_USER_SUCCESS, payload: user })
+      });
   }
 }
