0a6176b718f592c9f6cea0efac167e0ccce8cf46
John Crisostomo <johncrisostomo@protonmail.ch>
2017-05-28 15:05:17 +0800

    finalizes login / sign up functionalities

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { Text } from 'react-native';
 import { connect } from 'react-redux';
-import { Card, CardSection, Input, Button } from './common';
+import { Card, CardSection, Input, Button, Spinner } from './common';
 import { emailChanged, passwordChanged, loginUser } from '../actions';
 
 class LoginForm extends Component {
@@ -17,6 +17,18 @@ class LoginForm extends Component {
     const { email, password } = this.props;
 
     this.props.loginUser({ email, password });
+  }
+
+  renderButton() {
+    if (this.props.loading) {
+      return <Spinner size={'large'} />
+    }
+
+    return (
+      <Button onPress={this.onButtonPress.bind(this)}>
+        Login
+      </Button>
+    );
   }
 
   render() {
@@ -46,7 +58,7 @@ class LoginForm extends Component {
         </Text>
 
         <CardSection>
-          <Button onPress={this.onButtonPress.bind(this)}>Login</Button>
+          {this.renderButton()}
         </CardSection>
       </Card>
     );
@@ -62,8 +74,8 @@ const styles = {
 };
 
 const mapStateToProps = ({ auth }) => {
-  const { email, password, error} = auth;
-  return { email, password, error };
+  const { email, password, error, loading } = auth;
+  return { email, password, error, loading };
 }
 
 export default connect(mapStateToProps, {
