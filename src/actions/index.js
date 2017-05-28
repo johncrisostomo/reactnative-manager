da57122f7950fc717672c7b069751348820b2ac3
John Crisostomo <johncrisostomo@protonmail.ch>
2017-05-28 16:02:24 +0800

    uses actions from RNRF to programmatically navigate

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,4 +1,6 @@
 import firebase from 'firebase';
+import { Actions } from 'react-native-router-flux';
+
 import {
   EMAIL_CHANGED,
   PASSWORD_CHANGED,
@@ -39,6 +41,8 @@ const loginUserSuccess = (dispatch, user) => {
     type: LOGIN_USER_SUCCESS,
     payload: user,
   });
+
+  Actions.employeeList();
 };
 
 const loginUserFail = (dispatch) => {
