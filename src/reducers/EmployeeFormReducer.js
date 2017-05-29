1d07765e60285bda73d188f6e3296a564cc7febc
John Crisostomo <johncrisostomo@protonmail.ch>
2017-05-29 18:25:58 +0800

    clears form every successful save

--- src/reducers/EmployeeFormReducer.js
+++ src/reducers/EmployeeFormReducer.js
@@ -1,4 +1,8 @@
-import { EMPLOYEE_UPDATE, EMPLOYEE_CREATE } from '../actions/types';
+import {
+  EMPLOYEE_UPDATE,
+  EMPLOYEE_CREATE,
+  EMPLOYEE_SAVE_SUCCESS
+} from '../actions/types';
 
 const INITIAL_STATE = { name: '', phone: '', shift: '' };
 
@@ -7,7 +11,9 @@ export default (state = INITIAL_STATE, action) => {
     case EMPLOYEE_UPDATE:
       return { ...state, [action.payload.prop]: action.payload.value };
     case EMPLOYEE_CREATE:
-      return INITIAL_STATE; 
+      return INITIAL_STATE;
+    case EMPLOYEE_SAVE_SUCCESS:
+      return INITIAL_STATE;
     default:
       return state;
   }
